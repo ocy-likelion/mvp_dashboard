110ff8035998f259fa9cc873d1ab2804b1fc81ba
streqmin <sanghoo1023@gmail.com>
2025-08-08 11:05:39 +0900

    chore: DB 환경변수 참조 코드 정리

--- app/config.py
+++ app/config.py
@@ -6,8 +6,6 @@ load_dotenv()
 
 # PostgreSQL 데이터베이스 연결 설정
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    DATABASE_URL = "postgresql://cysss:QJxyP6VuLMAZykzMyeRtO3QJUGMf0aWA@dpg-cuim9pogph6c73acoj0g-a/mvp_dashboard"
 
 # 서버 포트 설정
 PORT = int(os.getenv("PORT", 10000))  # 기본값을 10000으로 설정
