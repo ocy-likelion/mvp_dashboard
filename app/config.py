2b0da1bfef5a2c4bb4c0bb65a5b2acde142e9cf8
streqmin <sanghoo1023@gmail.com>
2025-08-12 09:56:27 +0900

    feat: SQLAlchemy 환경설정

--- app/config.py
+++ app/config.py
@@ -7,5 +7,15 @@ load_dotenv()
 # PostgreSQL 데이터베이스 연결 설정
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# SQLAlchemy 설정
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_size": 10,
+    "pool_recycle": 3600,
+    "pool_pre_ping": True,
+    "max_overflow": 20,
+}
+
 # 서버 포트 설정
 PORT = int(os.getenv("PORT", 10000))  # 기본값을 10000으로 설정
