69a568d1b623c242c4a9f184b0d5e4414fd47a50
streqmin <sanghoo1023@gmail.com>
2025-09-03 09:39:52 +0900

    chore: JS 에서 쿠키 접근 가능하도록 보안 해제

--- app/__init__.py
+++ app/__init__.py
@@ -41,7 +41,7 @@ def create_app():
     # 세션 설정 강화
     app.config.update(
         SESSION_COOKIE_SECURE=True,  # HTTPS에서만 쿠키 전송
-        SESSION_COOKIE_HTTPONLY=True,  # JavaScript에서 쿠키 접근 방지
+        # SESSION_COOKIE_HTTPONLY=True,  # JavaScript에서 쿠키 접근 방지
         SESSION_COOKIE_SAMESITE="Lax",  # CSRF 공격 방지
         PERMANENT_SESSION_LIFETIME=timedelta(
             hours=12
